a375ddc3a96e23b9aab4a903ce996913315e5c54
rbottiglia <renatabottiglia@gmail.com>
2019-12-20 19:17:29 -0300

    Projeto final do curso online de front-end da Reprograma

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function minhaFuncao(){
+let elemento = document.querySelector(".maravilhosa__bio");
+let paragrafo = document.createElement('p');
+let texto = 'The Voyage Out (1915) / Modern Fiction (1919) / Night and Day (1919) / O Quarto de Jacob (1922) / O Leitor Comum (1925) / Mrs. Dalloway (1925) / To the lighthouse (1927) / Orlando: A Biography (1928) / Um Teto Todo Seu (1929) / On Being III (1930) / The London Scene (1931) / As Ondas (1931) / The Common Reader: Second Series (1932) / Flush: Uma Biografia (1933) / Travels With Virginia Woolf (1933) / Os Anos (1937) / Três Guinéus (1938) / Entre os Atos (1941) / The Death of the Moth and Other Essays (1942) / The Moment and Other Essays (1947) / The Captains Death Bed and Other Essays (1950)/ A Writers Diary (1953 - trechos do diário integral) / Granite and Rainbow (1958) / Momentos de Vida (1976) / Freshwater: A Comedy (performado em 1923, revisado em 1935 e publicado em 1976) /  Books and Portraits (1978) / Women and Writing (1979) / A Moments Liberty: the shorter diary (1990) / Paper Darts: The Illustrated Letters of Virginia Woolf (1991) / Congenial Spirits: The Selected Letters (1993) / The Letters of Virginia Woolf, 1888-1941 (1993) / The Platform of Time: Memoirs of Family and Friends, Expanded Edition (2008) / Passionate Apprentice: The Early Journals, 1897-1909 (2016)';
+paragrafo.innerText = texto;
+elemento.appendChild(paragrafo);
+}
+
